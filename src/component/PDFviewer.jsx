2a5a942d21b4846f0a7021b352bed27f31fb9deb
zinkki <kkokijinkki@naver.com>
2023-01-11 12:32:23 +0900

    rebuild

--- src/component/PDFviewer.jsx
+++ src/component/PDFviewer.jsx
@@ -1,5 +1,5 @@
 import Button from 'react-bootstrap/Button';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
