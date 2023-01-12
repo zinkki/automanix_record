a57a2d35e0bbf58563e0dee80278b3b996377546
zinkki <kkokijinkki@naver.com>
2023-01-12 12:30:15 +0900

    location - update

--- src/component/ButtonList.jsx
+++ src/component/ButtonList.jsx
@@ -2,10 +2,10 @@ import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 
 export default function List() {
-
+  //window.location.origin + '/pdf'
   return(
     <>
-    <Button variant="success" style={{marginRight:'0.5rem'}} onClick={()=>{window.open('/pdf')}}>Day1</Button>
+    <Button variant="success" style={{marginRight:'0.5rem'}} onClick={()=>{window.open(window.location.origin+'/pdf')}}>Day1</Button>
     <Button variant="secondary" style={{marginRight:'0.5rem'}}>Day2</Button>
     <Button variant="secondary" style={{marginRight:'0.5rem'}}>Day3</Button>
     <Button variant="secondary" style={{marginRight:'0.5rem'}}>Day4</Button>
