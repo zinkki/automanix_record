245a429f848fd9006103654907730acf1808fe7f
zinkki <kkokijinkki@naver.com>
2023-01-14 23:26:03 +0900

    table-update

--- src/component/RankingChart.jsx
+++ src/component/RankingChart.jsx
@@ -2,9 +2,9 @@ import Table from 'react-bootstrap/Table';
 
 export default function RankingChart() {
   return(
-    <Table hover>
+    <Table hover style={{border:'1rem solid #fff'}} className='text-center ranking-chart'>
       <thead>
-        <tr>
+        <tr style={{backgroundColor:'#fff'}}>
           <th>순위</th>
           <th>이름</th>
           <th>회사명</th>
@@ -13,75 +13,75 @@ export default function RankingChart() {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>홍길동</td>
-          <td>홍길동 컴퍼니</td>
-          <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+        <tr style={{color:'#efefef'}}>
+          <td style={{backgroundColor:'#d44428',color:'#fff',fontWeight:'900'}}>1</td>
+          <td style={{color:'#efefef'}}>홍길동</td>
+          <td style={{color:'#efefef'}}>홍길동 컴퍼니</td>
+          <td style={{color:'#efefef'}}>모터스포츠팀</td>
+					<td style={{backgroundColor:'#d44428',fontWeight:'900'}}>1:49.544</td>
         </tr>
         <tr>
-					<td>2</td>
+					<td style={{backgroundColor:'#b0c9e3',color:'#222',fontWeight:'900'}}>2</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td style={{backgroundColor:'#b0c9e3',color:'#222',fontWeight:'900'}}>1:49.544</td>
         </tr>
         <tr>
-					<td>3</td>
+					<td style={{backgroundColor:'#b0c9e3',color:'#222',fontWeight:'900'}}>3</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td style={{backgroundColor:'#b0c9e3',color:'#222',fontWeight:'900'}}>1:49.544</td>
         </tr>
 				<tr>
-					<td>4</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>4</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
 				<tr>
-					<td>5</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>5</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
 				<tr>
-					<td>6</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>6</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
 				<tr>
-					<td>7</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>7</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
 				<tr>
-					<td>8</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>8</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
 				<tr>
-					<td>9</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>9</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
 				<tr>
-					<td>10</td>
+					<td style={{backgroundColor:'#222',color:'#fff',fontWeight:'900'}}>10</td>
           <td>홍길동</td>
           <td>홍길동 컴퍼니</td>
           <td>모터스포츠팀</td>
-					<td>1:49.544</td>
+					<td className='black'>1:49.544</td>
         </tr>
       </tbody>
     </Table>
