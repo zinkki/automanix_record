e24bdaa06ab4fc762060312e58a45af0ac4c0458
zinkki <kkokijinkki@naver.com>
2023-01-18 04:09:58 +0900

    ///

--- src/component/RankingTab.jsx
+++ src/component/RankingTab.jsx
@@ -6,7 +6,7 @@ export default function RankingTab() {
   const title = <div className='labtimeTitle'>베스트 랩 타임10</div>;
 	return(
 		<>
-		<img className="ranking-left mt-6" src={require("../asset/img/labtime.png")} alt="..." />
+		<img className="ranking-left" src={require("../asset/img/labtime.png")} alt="..." />
 		<div className='mt-5 ranking-width'>
 			<Tabs defaultActiveKey="home" id="fill-tab-example">
 				<Tab eventKey="home" title={title} className='bg-white'>
--- src/server/config/db.js
+++ src/server/config/db.js
@@ -1,10 +1,10 @@
-var mysql = require('mysql');
-const db = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	port: process.env.DB_PORT
-})
+// var mysql = require('mysql');
+// const db = mysql.createPool({
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	database: process.env.DB_DATABASE,
+// 	port: process.env.DB_PORT
+// })
 
-module.exports = db;
+// module.exports = db;
